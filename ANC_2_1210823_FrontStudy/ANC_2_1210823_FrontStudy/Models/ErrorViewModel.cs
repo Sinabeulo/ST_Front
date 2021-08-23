250d36e8dc5117b68e06424fd6401edc869b40f2
MinJihong <minjih0725@gmail.com>
2021-08-23 20:21:59 +0900

    Init Commit

---
+++ ANC_2_1210823_FrontStudy/ANC_2_1210823_FrontStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ANC_2_1210823_FrontStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
